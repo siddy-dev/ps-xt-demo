dfcb460f21037b936c05a31eec4245354075e78c
Siddharth Yadav <siddybuddy18@gmail.com>
2021-01-28 21:22:53 +0530

    changing scripts and spi response limit increased to 100

--- src/api/programs.js
+++ src/api/programs.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export function getPrograms(year, launchVal, landingVal) {
-  let url = "https://api.spaceXdata.com/v3/launches?limit=10";
+  let url = "https://api.spaceXdata.com/v3/launches?limit=100";
   year !== null && (url += `&launch_year=${year}`);
   launchVal !== null && (url += `&launch_success=${launchVal}`);
   landingVal !== null && (url += `&land_success=${landingVal}`);
